aa217de220c1201b5c19b1f1faf939bd0bd62fd6
Nigini Abilio <nigini@gmail.com>
2013-07-26 16:01:29 -0700

    Second version and final version from tutorial, now with database and stylesheet.

--- src/guestbook/SignGuestbookServlet.java
+++ src/guestbook/SignGuestbookServlet.java
@@ -1,9 +1,15 @@
 package guestbook;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import java.io.IOException;
+import java.util.Date;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author nigini
+ * @author Nigini Abilio - nigini@gmail.com
  */
 public class SignGuestbookServlet extends HttpServlet {
     private static final Logger log = Logger.getLogger(SignGuestbookServlet.class.getName());
@@ -22,17 +28,20 @@ public class SignGuestbookServlet extends HttpServlet {
         UserService userService = UserServiceFactory.getUserService();
         User user = userService.getCurrentUser();
         
+        String guestbookName = req.getParameter("guestbookName");
+        Key guestbookKey = KeyFactory.createKey("Guestbook", guestbookName);
         String content = req.getParameter("content");
-        if(content == null){
-            content = "(NO GREETINGS!!! THAT'S SAD!)";
-        }
-        if(user != null){
-            log.info("Greeting posted by user " + user.getNickname() + ": " + content);
-        } else {
-            log.info("Greeting posted anonymously: " + content);
-        }
-        resp.sendRedirect("/guestbook.jsp");
+        Date date = new Date();
+        
+        Entity greeting = new Entity("Greeting", guestbookKey);
+        greeting.setProperty("user", user);
+        greeting.setProperty("date", date);
+        greeting.setProperty("content", content);
+        
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(greeting);
+        
+        resp.sendRedirect("/guestbook.jsp?guestbookName=" + guestbookName);
     }
-    
-    
+
 }
